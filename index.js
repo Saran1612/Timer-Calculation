97dd9b962d4bc0e8d4b7db4d085cfec7a81f2f6c
Saran1612 <99238136+Saran1612@users.noreply.github.com>
2023-08-25 10:37:37 +0530

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ import * as ReactDOM from 'react-dom/client';
 import { StyledEngineProvider } from '@mui/material/styles';
 import Timer from './Timer';
 import Calculator from './calculator';
+import Todo from './todo.js';
 
 ReactDOM.createRoot(document.querySelector('#root')).render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
+      <Todo />
+      <Timer />
       <Calculator />
     </StyledEngineProvider>
   </React.StrictMode>
---
+++ todo.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+
+const Todo = () => {
+  const [data, setData] = useState('');
+  const [dataArr, setDataArr] = useState([]);
+  const [ID, setId] = useState(1);
+
+  const handleChange = (event) => {
+    setData(event.target.value);
+  };
+
+  const handleAdd = () => {
+    setData('');
+    setId((prev) => prev + 1);
+    setDataArr((prev) => [...prev, { id: ID, task: data }]);
+  };
+
+  const handleEdit = (id) => {
+    console.log(id, 'editedOne');
+    const needToEdit = dataArr.filter((item) => Number(item.id) === Number(id));
+    setData(needToEdit[0].task);
+    console.log(needToEdit, 'needToEdit');
+  };
+
+  const handleDelete = (id) => {
+    console.log(id, 'deletedOne');
+    const needToEdit = dataArr.filter((item) => Number(item.id) !== Number(id));
+    setDataArr(needToEdit);
+  };
+
+  return (
+    <>
+      <div>
+        <input type="text" onChange={handleChange} value={data} />
+        <button onClick={handleAdd}>Add</button>
+      </div>
+
+      <div>
+        <ul>
+          {/* {dataArr.map((item, index) => (
+            <li
+              key={index}
+              style={{ display: 'flex', justifyContent: 'start' }}
+            >
+              <p>{item.task} </p>
+              <button onClick={() => handleEdit(item.id)}>Edit</button>
+              <button onClick={() => handleDelete(item.id)}>Delete</button>
+            </li>
+          ))} */}
+          {dataArr.map((item) => (
+            <li
+              key={item.id}
+              style={{ display: 'flex', justifyContent: 'start' }}
+            >
+              <p>{item.task} </p>
+              <button onClick={() => handleEdit(item.id)}>
+                {ID === item.id ? 'Update' : 'Edit'}
+              </button>
+              <button onClick={() => handleDelete(item.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Todo;
